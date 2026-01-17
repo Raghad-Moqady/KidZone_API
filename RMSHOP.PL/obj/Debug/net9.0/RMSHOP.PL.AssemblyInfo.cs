63a8060e10718c178ed3939963916ca668da33ee
Raghad-Moqady <raghadmoqady@gmail.com>
2026-01-17 21:04:14 +0200

    TODO: Refactor UpdateCategoryAsync to avoid using DbContext.Update()

--- RMSHOP.PL/obj/Debug/net9.0/RMSHOP.PL.AssemblyInfo.cs
+++ RMSHOP.PL/obj/Debug/net9.0/RMSHOP.PL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RMSHOP.PL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b5596177a56e44e2256612be0d2be7ce18ab35f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3bb5417bdadeb917a0ba58b8bca4bfbed4e9cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RMSHOP.PL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RMSHOP.PL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
