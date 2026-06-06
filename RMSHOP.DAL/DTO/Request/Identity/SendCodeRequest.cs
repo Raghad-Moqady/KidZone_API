9fd00ef72966beb3816a7965333617eeb8ae1407
Raghad-Moqady <raghadmoqady@gmail.com>
2026-06-06 20:20:27 +0300

    Send code: add validation to SendCodeRequest DTO

--- RMSHOP.DAL/DTO/Request/Identity/SendCodeRequest.cs
+++ RMSHOP.DAL/DTO/Request/Identity/SendCodeRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace RMSHOP.DAL.DTO.Request.Identity
 {
     public class SendCodeRequest
     {
+        [Required(ErrorMessage = "Email Is Required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Format")]
         public string Email { get; set; }
     }
 }
